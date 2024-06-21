9fd853a20d2b4ed0ef67e4cc08e2eb9ac056b131
Marcio Lucas <48500194+MarcioLucas22@users.noreply.github.com>
2024-06-21 16:13:05 -0300

    edit entry

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Topic
+from .models import Topic, Entry
 from django.http import HttpRequest, HttpResponseRedirect
 from .forms import TopicForm, EntryForm
 from django.urls import reverse
@@ -54,7 +54,28 @@ def new_entry(request: HttpRequest, topic_id):
             new_entry = form.save(commit=False) # Cria um novo objeto mas não salva no BD
             new_entry.topic = topic
             new_entry.save()
-            return HttpResponseRedirect(reverse('topic'), args=[topic_id])
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
     
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request: HttpRequest, entry_id):
+    '''Edita uma entrada existente'''
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Requisição inicial; preenche previamente o formulário com a entrada atual
+        form = EntryForm(instance=entry) # Esse instance serve para trazer o formulário já preenchido
+    else:
+        # Dados de post submetidos; processa os dados
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic.id]))
+        
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
+
+
+
